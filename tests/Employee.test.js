5aaac37c20d7853b9a133a6058c72d4b14158312
Samuel <samuelyongw@gmail.com>
2022-09-30 10:47:17 +1000

    Implemented negative/false tests for employee which checks if inputted value is valid or not

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -4,18 +4,22 @@ describe("Employee class", () => {
 
     const employee = new Employee("Sam", 21, "samuel@gmail.com");
 
-    const falseEmployee = new Employee(349, "Hello", 0253)
+    const falseEmployee = new Employee(214,"sfaf", "sammm")
 
     describe("getName method", () => {
         it("returns employee's name on the console", () => expect(employee.getName()).toEqual("Sam"));
+        it("returns an error", () => expect(falseEmployee.getName()).toBe(undefined));
     });
     describe("getId method", () => {
         it("returns employee's ID on the console", () => expect(employee.getId()).toEqual(21));
+        it("returns an error", () => expect(falseEmployee.getId()).toBe(undefined));
     });
     describe("getEmail method", () => {
         it("returns employee's email", () => expect(employee.getEmail()).toEqual("samuel@gmail.com"));
+        it("returns an error", () => expect(falseEmployee.getEmail()).toBe(undefined));
     });
     describe("getRole method", () => {
         it("returns employee's role", () => expect(employee.getRole()).toEqual("Employee"));
+        it("returns an error", () => expect(falseEmployee.getRole()).toBe(undefined));
     });
 });
