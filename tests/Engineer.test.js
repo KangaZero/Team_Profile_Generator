cdc5e805a016e956d96872be19f96dc1e3942b62
Samuel <samuelyongw@gmail.com>
2022-10-05 21:35:20 +1100

    fixed test error for Manager office number

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -11,18 +11,6 @@ describe("Engineer class", () => {
         it("returns user's github username", () => {
             expect(engineer.getGithub()).toBe("KangaZero");
 
-            //TODO fix this
-            // const result = "KangaZero";
-            // const mock = jest.spyOn(console, "log");
-            // mock.mockImplementation(() => {});
-
-            // engineer.getGithub();
-            // console.log(engineer.getGithub())
-        
-            // expect(mock).toBe(result);
-            
-            //  mock.mockRestore();
-        
         });
     
         });
--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -4,7 +4,10 @@ describe("Manager class", () =>{
     describe("getOfficeNumber method", () => {
         it("returns manager's office number", () => {
             const manager = new Manager("Sam", 21, 'samuelyongw@gmail.com', 25);
+            const fakeManager = new Manager("Sam", 21, 'samuelyongw@gmail.com', "25");
+            
             expect(manager.getOfficeNumber()).toBe(25);
+            expect(fakeManager.getOfficeNumber()).toBe(undefined);
         });
     });
 })
